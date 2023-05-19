a65d24841ef9769039b3c2dc8acaec277530980b
kvrp97 <ravinduprabhashwara97@gmail.com>
2023-05-19 15:12:43 +0530

    base url changed

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ import App from './App';
 import {name as appName} from './app.json';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://192.168.8.190:8091/';
+axios.defaults.baseURL = 'http://13.126.175.133:8080/note-collector-app-api-0.0.1-SNAPSHOT/';
 
 AppRegistry.registerComponent(appName, () => App);
