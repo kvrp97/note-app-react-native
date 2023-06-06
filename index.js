32b4217ffc4b1997179d041877c9323d3c355bc9
kvrp97 <ravinduprabhashwara97@gmail.com>
2023-06-06 12:27:36 +0530

    error msg changed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import App from './App';
 import {name as appName} from './app.json';
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'http://13.126.175.133:8080/note-collector-app-api-0.0.1-SNAPSHOT/';
+// axios.defaults.baseURL = 'http://65.1.2.217:8080/note-collector-app-api-0.0.1-SNAPSHOT/';
 axios.defaults.baseURL = 'http://192.168.8.190:8091/';
 
 AppRegistry.registerComponent(appName, () => App);
